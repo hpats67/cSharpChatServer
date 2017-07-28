431b3fb37e3b2f0dd5e0a636f7e222d92b9eff13
Steven <hpats67@gmail.com>
2017-07-28 13:48:21 -0700

    fixed form display

--- chatclient/chatClient/Form1.cs
+++ chatclient/chatClient/Form1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Net.Sockets;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
@@ -17,6 +12,10 @@ namespace chatClient
         NetworkStream serverStream = default(NetworkStream);
         string readData = null;
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
         private void Button1_Click(object sender, EventArgs e)
         {
