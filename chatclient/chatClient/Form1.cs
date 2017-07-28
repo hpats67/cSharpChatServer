6ed9ca74eb7d3012a58f857c5acda877644e98a9
Steven <hpats67@gmail.com>
2017-07-28 13:33:50 -0700

    finish designing form

---
+++ chatclient/chatClient/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace chatClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientSocket = new TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            byte[] outStream = Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            readData = "Connected to Chat Server...";
+            Msg();
+            clientSocket.Connect("10.0.0.95", 3500);
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = Encoding.ASCII.GetBytes(textBox3.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread ctThread = new Thread(GetMessage);
+            ctThread.Start();
+        }
+
+        private void GetMessage()
+        {
+            while ((true))
+            {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+                Msg();
+            }
+        }
+
+        private void Msg()
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(Msg));
+            else
+                textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + readData;
+        }
+
+    }
+}
---
+++ chatclient/chatClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace chatClient
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
